03388642136d70a7056bdc0a7f2d750dffa3c47f
Magnus <EnderCrypt@gmail.com>
2015-10-29 02:43:30 +0100

    made the test example a bit clearer

--- src/com/github/endercrypt/test/Main.java
+++ src/com/github/endercrypt/test/Main.java
@@ -1,20 +1,36 @@
 package com.github.endercrypt.test;
 
+import java.util.TreeSet;
+
 import com.github.endercrypt.scoredSet.ScoredSet;
 
 public class Main
 {
-	public static int counter = 0;
+	public static int counter;
 	public static void main(String[] args)
 	{
+		// SCOREDSET
+		counter = 0;
 		ScoredSet<TestData> ss = new ScoredSet<>();
-		for (int i=0;i<5;i++)
-		{
-			int num = (int) (Math.random()*100);
-			ss.add(new TestData(num));
-		}
-		System.out.println(ss+" Size: "+ss.size());
-		System.out.println(ss.remove(1));
-		System.out.println(ss+" Size: "+ss.size());
+		ss.add(new TestData(0));
+		ss.add(new TestData(-50));
+		ss.add(new TestData(100));
+		ss.add(new TestData(50));
+		ss.add(new TestData(50));
+		System.out.println(ss);
+		
+		//> [(3. s100), (5. s50), (4. s50), (1. s0), (2. s-50)]
+		
+		// TREESET
+		counter = 0;
+		TreeSet<TestData> ts = new TreeSet<>();
+		ts.add(new TestData(0));
+		ts.add(new TestData(-50));
+		ts.add(new TestData(100));
+		ts.add(new TestData(50));
+		ts.add(new TestData(50));
+		System.out.println(ts);
+		
+		//> [(3. s100), (4. s50), (1. s0), (2. s-50)]
 	}
 }
--- src/com/github/endercrypt/test/TestData.java
+++ src/com/github/endercrypt/test/TestData.java
@@ -1,7 +1,7 @@
 package com.github.endercrypt.test;
 import com.github.endercrypt.scoredSet.Scoreable;
 
-public class TestData implements Scoreable
+public class TestData implements Scoreable, Comparable<TestData>
 {
 	private int id = ++Main.counter;
 	private int score;
@@ -18,5 +18,10 @@ public class TestData implements Scoreable
 	public String toString()
 	{
 		return "("+id+". s"+score+")";
+	}
+	@Override
+	public int compareTo(TestData other)
+	{
+		return (other.score-score);
 	}
 }
